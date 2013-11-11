3950193f8989f7f913f4161dc256cdb01595bbd2
Tranz5 <Tranz5@users.noreply.github.com>
2013-11-10 23:12:20 -0500

    Pre Release of MW2

--- src/version.cpp
+++ src/version.cpp
@@ -36,8 +36,8 @@ const std::string CLIENT_NAME("HoboNickels");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE 
-#    define GIT_COMMIT_ID "eb597e5"
-#    define GIT_COMMIT_DATE "Sun Nov 03 18:40:53 2013"
+#    define GIT_COMMIT_ID "90998b4"
+#    define GIT_COMMIT_DATE "Sun Nov 10 23:11:25 2013"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
