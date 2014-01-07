efe2f0f92ccdb0466a9e6eb18c267bafd9f01aec
Tranz5 <Tranz5@users.noreply.github.com>
2014-01-07 18:52:32 -0500

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -37,10 +37,9 @@ const std::string CLIENT_NAME("HoboNickels");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE 
 
-#    define GIT_COMMIT_ID "e43cb68"
+#    define GIT_COMMIT_ID "a6ca3a92"
 
-
-#    define GIT_COMMIT_DATE "Sat Dec 14 14:19:45 2013"
+#    define GIT_COMMIT_DATE "Tue Jan 07 18:52:00 2014"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
