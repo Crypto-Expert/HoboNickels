2fd8e8f2e3ba3d52863361da34b4ec31f9c7f3bc
Tranz5 <Tranz5@users.noreply.github.com>
2014-01-07 16:14:37 -0500

    V1.3
    getLastBlockDate fix

--- src/qt/clientmodel.cpp
+++ src/qt/clientmodel.cpp
@@ -49,7 +49,10 @@ int ClientModel::getNumBlocksAtStartup()
 
 QDateTime ClientModel::getLastBlockDate() const
 {
-    return QDateTime::fromTime_t(pindexBest->GetBlockTime());
+    if (pindexBest)
+      return QDateTime::fromTime_t(pindexBest->GetBlockTime());
+    else
+      return QDateTime::fromTime_t(1374635824); // Genesis block's time
 }
 
 void ClientModel::updateTimer()
