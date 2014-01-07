d923ea1b5ba63a2fdeeb3920628c2dc8231e5eb2
Tranz5 <Tranz5@users.noreply.github.com>
2014-01-07 16:53:58 -0500

    V1.3
    checkpoint policy

--- src/checkpoints.h
+++ src/checkpoints.h
@@ -10,6 +10,12 @@
 
 #define CHECKPOINT_MAX_SPAN (60 * 60 * 4) // max 4 hours before latest block
 
+#ifdef WIN32
+#undef STRICT
+#undef PERMISSIVE
+#undef ADVISORY
+#endif
+
 class uint256;
 class CBlockIndex;
 class CSyncCheckpoint;
@@ -19,6 +25,18 @@ class CSyncCheckpoint;
  */
 namespace Checkpoints
 {
+
+    /** Checkpointing mode */
+    enum CPMode
+    {
+      // Scrict checkpoints policy, perform conflicts verification and resolve conflicts
+      STRICT = 0,
+      // Advisory checkpoints policy, perform conflicts verification but don't try to resolve them
+      ADVISORY = 1,
+      // Permissive checkpoints policy, don't perform any checking
+      PERMISSIVE = 2
+    };
+
     // Returns true if block passes checkpoint checks
     bool CheckHardened(int nHeight, const uint256& hash);
 
