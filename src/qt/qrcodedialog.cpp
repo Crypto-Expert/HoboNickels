9fe4e0d2f8ee30c59b07d409b3dd1730ec4e9a23
Tranz5 <Tranz5@users.noreply.github.com>
2014-01-07 16:13:45 -0500

    V1.3 URI Fix

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -83,7 +83,7 @@ void QRCodeDialog::genCode()
 
 QString QRCodeDialog::getURI()
 {
-    QString ret = QString("bitcoin:%1").arg(address);
+    QString ret = QString("hobonickels:%1").arg(address);
     int paramCount = 0;
 
     ui->outUri->clear();
@@ -92,7 +92,7 @@ QString QRCodeDialog::getURI()
     {
         if (ui->lnReqAmount->validate())
         {
-            // even if we allow a non BTC unit input in lnReqAmount, we generate the URI with BTC as unit (as defined in BIP21)
+            // even if we allow a non HBN unit input in lnReqAmount, we generate the URI with HBN as unit (as defined in BIP21)
             ret += QString("?amount=%1").arg(BitcoinUnits::format(BitcoinUnits::BTC, ui->lnReqAmount->value()));
             paramCount++;
         }
