dea1630960d16794e4aecdbae728e5e624252460
Tranz5 <Tranz5@users.noreply.github.com>
2014-02-08 16:43:41 -0500

    Version displayed at startup

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -86,7 +86,7 @@ static void InitMessage(const std::string &message)
 {
     if(splashref)
     {
-        splashref->showMessage(QString::fromStdString(message), Qt::AlignBottom|Qt::AlignHCenter, QColor(255,255,200));
+        splashref->showMessage(QString::fromStdString(message+'\n'+'\n') + QString::fromStdString(FormatFullVersion().c_str()), Qt::AlignBottom|Qt::AlignHCenter, QColor(255,255,200));
         QApplication::instance()->processEvents();
     }
 }
