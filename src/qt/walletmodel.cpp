e19ab50998f57f137759f8f797da656b749c8caa
Tranz5 <Tranz5@users.noreply.github.com>
2014-02-07 20:44:37 -0500

    fWalletUnlockMintOnly Extended to GUI

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -360,16 +360,26 @@ void WalletModel::unsubscribeFromCoreSignals()
 // WalletModel::UnlockContext implementation
 WalletModel::UnlockContext WalletModel::requestUnlock()
 {
+
     bool was_locked = getEncryptionStatus() == Locked;
+
+    if ((!was_locked) && fWalletUnlockMintOnly)
+    {
+       setWalletLocked(true);
+       was_locked = getEncryptionStatus() == Locked;
+
+    }
+
     if(was_locked)
     {
         // Request UI to unlock wallet
         emit requireUnlock();
     }
+
     // If wallet is still locked, unlock was failed or cancelled, mark context as invalid
     bool valid = getEncryptionStatus() != Locked;
 
-    return UnlockContext(this, valid, was_locked);
+    return UnlockContext(this, valid, was_locked && !fWalletUnlockMintOnly);
 }
 
 WalletModel::UnlockContext::UnlockContext(WalletModel *wallet, bool valid, bool relock):
