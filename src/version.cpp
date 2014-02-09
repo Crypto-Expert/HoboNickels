d93cfc70f346e13d61ee014b238d59aa62bd44e7
Tranz5 <Tranz5@users.noreply.github.com>
2014-02-09 14:32:19 -0500

    Removed accidental gitid paste

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("HoboNickels");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE 
-c722db8
+
 #    define GIT_COMMIT_ID "c722bd8"
 
 #    define GIT_COMMIT_DATE "Sun Feb 09 14:18:00 2014"
