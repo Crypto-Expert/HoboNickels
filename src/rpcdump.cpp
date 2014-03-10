a212a3a984fde5e05bb107524ae97ae1517b876d
Tranz5 <Tranz5@users.noreply.github.com>
2014-03-09 20:44:39 -0400

    removed iostream and fstream

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -2,9 +2,6 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <iostream>
-#include <fstream>
-
 #include "bitcoinrpc.h"
 #include "ui_interface.h"
 #include "base58.h"
