ba85423ba59801e1ba6d9a4a8130cc7a7a8d059a
Tranz5 <Tranz5@users.noreply.github.com>
2014-07-21 22:08:22 -0400

    Further updates to CNetMessage processing
    These could be combined with the previous commit

--- src/net.h
+++ src/net.h
@@ -203,7 +203,7 @@ public:
     CDataStream vSend;
     CCriticalSection cs_vSend;
 
-    std::vector<CNetMessage> vRecvMsg;
+    std::deque<CNetMessage> vRecvMsg;
     CCriticalSection cs_vRecvMsg;
     int nRecvVersion;
 
@@ -322,8 +322,8 @@ public:
     unsigned int GetTotalRecvSize()
     {
         unsigned int total = 0;
-        for (unsigned int i = 0; i < vRecvMsg.size(); i++)
-            total += vRecvMsg[i].vRecv.size();
+        BOOST_FOREACH(const CNetMessage &msg, vRecvMsg)
+            total += msg.vRecv.size() + 24;
         return total;
     }
 
@@ -334,8 +334,8 @@ public:
     void SetRecvVersion(int nVersionIn)
     {
         nRecvVersion = nVersionIn;
-        for (unsigned int i = 0; i < vRecvMsg.size(); i++)
-            vRecvMsg[i].SetVersion(nVersionIn);
+        BOOST_FOREACH(CNetMessage &msg, vRecvMsg)
+            msg.SetVersion(nVersionIn);
     }
 
     CNode* AddRef()
