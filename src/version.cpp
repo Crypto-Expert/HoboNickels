bcf7d224524efe3cb4e1f403706916901ad35bd5
Tranz5 <Tranz5@users.noreply.github.com>
2017-08-06 23:35:41 -0400

    Version Update

--- src/version.cpp
+++ src/version.cpp
@@ -37,9 +37,9 @@ const std::string CLIENT_NAME("HoboNickels");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE 
 
-#    define GIT_COMMIT_ID "f46fa0c"
+#    define GIT_COMMIT_ID "87b0f55"
 
-#    define GIT_COMMIT_DATE "Mon Jun 01 22:01:01 2015"
+#    define GIT_COMMIT_DATE "Sun Aug 06 22:34:01 2017"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
